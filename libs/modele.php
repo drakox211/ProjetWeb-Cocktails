2a8b509600a1c73033fd37f8fbc100a0ce12470c
Naeferith <toto2020@hotmail.fr>
2018-09-13 01:25:12 +0200

    Remove unused functions

--- libs/modele.php
+++ libs/modele.php
@@ -117,8 +117,7 @@ function getRacineName($hierarchie) {
 
 //Recupere la liste de fils d'un ingredient. renvoie null si c'est une feuille.
 function getIngredientFils($hierarchie, $nom) {
-	foreach($hierarchie as $key => $value) {								//Parcours des ingredients
-		$key = addslashes($key);
+	foreach($hierarchie as $key => $value) {	
 		if ($key == $nom) {
 			foreach ($value as $categorie => $ingredients) {				//...on parcours ses relations pere-fils
 				if ($categorie == 'sous-categorie') return $ingredients;	//...si l'ingredient n'est pas une feuille, on retourne ses fils
@@ -126,12 +125,6 @@ function getIngredientFils($hierarchie, $nom) {
 		}
 	}
 	return null;															//L'ingrédient est une feuille
-}
-
-//Verifie la présence d'un ingredient dans la bdd
-function checkIngredient($nom) {
-	$sql = "SELECT * FROM ingredients WHERE nom = '$nom'";
-	return (SQLSelect($sql) !== false);
 }
 
 //Ajoute un nouveau chemin a un ingredient
@@ -150,21 +143,14 @@ function addIngredient($nom, $path) {
 	SQLInsert($sql);
 }
 
-function controlIngredient($ingredient, $path) {
-	if(checkIngredient($ingredient)) addIngredientPath($ingredient, $path);	//Dans le cas ou l'ingrédient est présent, cela veut dire que l'on a trouvé un autre path d'acces a cet ingredient
-	else addIngredient($ingredient, $path);									//Sinon on l'ajoute a la BDD
-}
-
 //Parcours donnees.ico.php pour inserer les ingredients dans la bdd avec leur arboressence.
 function parseData($dataTable, $nom, $path) {
 	//$dataTable est le tableau de données
 	//$hierarchie est un tableau des fils d'un ingredient
 	//Si $hierarchie est une feuille, on remonte
-	$nom = addslashes($nom);
-	controlIngredient($nom, $path);																//On ajoute l'ingredient
+	addIngredient(addslashes($nom), $path);														//On ajoute l'ingredient
 	$hierarchie = getIngredientFils($dataTable, $nom);
 	if (is_null($hierarchie)) return;
-	
 	
 	foreach ($hierarchie as $index => $ingredient) {											//Pour chaque ingredient fils
 		$localpath = $path.'.'.$index;															//On lui affecte le bon chemin
