8d660bee98b0e9502a603128349f91d8b65bf9c8
Naeferith <toto2020@hotmail.fr>
2018-09-14 23:07:16 +0200

    Use French only

--- templates/connexion.php
+++ templates/connexion.php
@@ -8,28 +8,25 @@ if (basename($_SERVER["PHP_SELF"]) != "index.php")
 }
 
 ?>
-<style>
-	body {background-color: #212529; color: white;}
-</style>
 
 <section id="connexion">
     <div class="container">
 	<form method="POST" action="controleur.php">
 	  <div class="form-group row">
-		<label for="inputPseudo" class="col-sm-2 col-form-label">Login</label>
+		<label for="inputPseudo" class="col-sm-2 col-form-label">Identifiant</label>
 		<div class="col-sm-10">
 		  <input type="text" name="pseudo" class="form-control" id="inputPseudo" placeholder="Pseudonyme">
 		</div>
 	  </div>
 	  <div class="form-group row">
-		<label for="inputPassword" class="col-sm-2 col-form-label">Password</label>
+		<label for="inputPassword" class="col-sm-2 col-form-label">Mot de passe</label>
 		<div class="col-sm-10">
-		  <input type="password" name="password" class="form-control" id="inputPassword" placeholder="Password">
+		  <input type="password" name="password" class="form-control" id="inputPassword" placeholder="Mot de passe">
 		</div>
 	  </div>
 	  <div class="form-group row">
 		<div class="col-sm-10">
-		  <button type="submit" name='action' value="Connexion" class="btn btn-primary">Log In</button>
+		  <button type="submit" name='action' value="Connexion" class="btn btn-primary">Connexion</button>
 		</div>
 	  </div>
 	  <div class="form-group row">
