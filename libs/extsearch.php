7c49beb59c733d05dfe5c19bf2d5724c985438a7
Naeferith <toto2020@hotmail.fr>
2018-09-17 16:57:41 +0200

    Prevent multiple ingredient search

--- libs/extsearch.php
+++ libs/extsearch.php
@@ -4,15 +4,16 @@ include_once "maLibUtils.php";
 include_once "modele.php";
 
 $action = valider("action");
-$value  = valider("value");
+$value  = valider("val");
+$mode   = valider("mode");
+
 switch($action){
 	case("loadBeginName"):
 		if (strlen($value) == 0) {
 			echo "false";
 			break;
 		}
-		$value .= '%';
-		$SQL = "SELECT * FROM ingredients WHERE nom LIKE '$value' GROUP BY NOM";
+		$SQL = ($mode == "true") ? "SELECT * FROM ingredients WHERE nom LIKE '".$value."%'" : "SELECT * FROM ingredients WHERE nom REGEXP '$value'";
 		$tab = parcoursRs(SQLSelect($SQL));
 		if($tab){ $tab = json_encode($tab, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES); echo $tab;}
 		else echo "false";
